c7dc6923491b565ad2162294b93ad226f170654d
maryjis <m.zubrikhina2014@yandex.ru>
2024-12-18 16:08:21 +0000

    Add ViTMAE training for RNA + fix training loop

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from src.utils import  * 
-from src.unimodal.trainer import UnimodalSurvivalTrainer
+from src.unimodal.trainer import UnimodalSurvivalTrainer,  UnimodalMAETrainer
 from pathlib import Path
+from transformers.models.vit_mae.configuration_vit_mae import ViTMAEConfig
 
 @hydra.main(version_base=None, config_path="src/configs", config_name="unimodal_config")
 def run(cfg : DictConfig) -> None:
@@ -13,14 +14,20 @@ def run(cfg : DictConfig) -> None:
     all_valid_metrics, all_test_metrics =[], []
     for fold_ind in range(cfg.base.splits):
         
+        cfg.base.save_path = f"outputs/models/{cfg.base.experiment_name}_split_{fold_ind}.pth"
         splits = load_splits(Path(cfg.base.data_path), fold_ind, cfg.base.remove_nan_column)
         
         if cfg.base.type == 'unimodal':
             # унимодальные (тут мы должны выбрать модальность) или мультимодальный + способ дообучения
-            trainer =UnimodalSurvivalTrainer(splits, cfg)
+            if cfg.base.strategy == "survival":
+                trainer = UnimodalSurvivalTrainer(splits, cfg)
+            elif cfg.base.strategy == "mae": 
+                trainer = UnimodalMAETrainer(splits, cfg)
+            else:
+                raise NotImplementedError(f"Such strategy - {cfg.base.strategy} isn't implemented.")   
         else:
             raise NotImplementedError("Now only unimodal training is implemented.")
-    
+
         valid_metrics =trainer.train(fold_ind)
         test_metrics =trainer.evaluate(fold_ind)
         all_valid_metrics.append(valid_metrics)
--- src/unimodal/trainer.py
+++ src/unimodal/trainer.py
@@ -1,9 +1,9 @@
 from typing import Dict, List, Tuple, Union
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 import pandas as pd
 from src.unimodal.rna.dataset import RNADataset
 from src.unimodal.rna.preprocessor import RNAPreprocessor
-from src.unimodal.rna.transforms import base_transforms
+from src.unimodal.rna.transforms import base_transforms, padded_transforms
 from torch.utils.data import Dataset, DataLoader
 from pycox.models.loss import NLLLogistiHazardLoss
 from torch.optim import AdamW
@@ -12,35 +12,75 @@ from src.unimodal.rna.encoder import initialise_rna_model
 import torch
 from ..evaluation import compute_survival_metrics
 import wandb
+from transformers.models.vit_mae.configuration_vit_mae import ViTMAEConfig
+from src.unimodal.rna.mae import RnaMAEForPreTraining
 
-class UnimodalSurvivalTrainer(object):
-    
+
+class Trainer(object):
     def __init__(self, splits: Dict[str,pd.DataFrame], cfg: DictConfig):
         
         self.cfg =cfg
-        self.datasets = self.initialise_datasets(splits)
+        self.initialise_preprocessing(splits)
+ 
+    def initialise_preprocessing(self, splits):
+         if self.cfg.base.modalities[0]=="rna":
+            self.preproc =RNAPreprocessor(splits["train"], self.cfg.base.rna_dataset_path, self.cfg.base.n_intervals, self.cfg.data.rna.is_cluster_genes , self.cfg.data.rna.clustering_threshold)
+            self.preproc.fit()
+         else:
+            raise NotImplementedError("Exist only for rna. Initialising datasets for other modalities aren't declared")    
+                
+    def initialise_datasets(self, splits, transforms):
+        datasets ={}
+        for split_name, dataset in splits.items():
+            splits[split_name] = self.preproc.transform_labels(dataset)
+            datasets[split_name] = RNADataset(splits[split_name], self.cfg.base.rna_dataset_path, 
+                                                 transform = transforms, is_hazard_logits = True, column_order=self.preproc.get_column_order())
+        return datasets
+   
+    
+    def train(self, fold_ind : int):
+    
+        for epoch in range(self.cfg.base.n_epochs):
+            self.model.train()
+            
+            train_metrics = self.__loop__("train",fold_ind, self.dataloaders['train'], self.cfg.base.device)
+            train_metrics.update({"epoch": epoch})
+            if self.cfg.base.log.logging:
+                wandb.log({f"train/fold_{fold_ind}/{key}" : value for key, value in train_metrics.items()})
+            
+            self.model.eval()
+            with torch.no_grad():    
+                val_metrics = self.__loop__("val",fold_ind, self.dataloaders['val'], self.cfg.base.device)
+            val_metrics.update({"epoch": epoch})    
+            if self.cfg.base.log.logging:
+                wandb.log({f"val/fold_{fold_ind}/{key}" : value for key, value in val_metrics.items()})
+        torch.save(self.model.state_dict(), self.cfg.base.save_path)
+        return val_metrics
+    
+    def evaluate(self, fold_ind : int):
+            self.model.eval()
+            with torch.no_grad():    
+                test_metrics = self.__loop__("test",fold_ind, self.dataloaders['test'], self.cfg.base.device)
+            if self.cfg.base.log.logging:
+                wandb.log({f"test/fold_{fold_ind}/{key}" : value for key, value in test_metrics.items()})    
+            return test_metrics    
+        
+
+class UnimodalSurvivalTrainer(Trainer):
+    
+    def __init__(self, splits: Dict[str,pd.DataFrame], cfg: DictConfig):
+        
+        super().__init__(splits, cfg)
+        transforms = base_transforms(self.preproc.get_scaling())
+        self.datasets = self.initialise_datasets(splits, transforms)
         self.dataloaders = {"train" : DataLoader(self.datasets["train"],shuffle=True, batch_size =cfg.base.batch_size),
                             "val" : DataLoader(self.datasets["val"],shuffle=False, batch_size = 1),
                             "test" : DataLoader(self.datasets["test"],shuffle=False, batch_size =1)
                             }
         self.model =self.initialise_models().to(cfg.base.device)
         self.initialise_loss()
+        print(self.model)
     
-    
-    
-    def initialise_datasets(self, splits):
-        datasets ={}
-        if self.cfg.base.modalities[0]=="rna":
-            self.preproc =RNAPreprocessor(splits["train"], self.cfg.base.rna_dataset_path, self.cfg.base.n_intervals, self.cfg.data.rna.is_cluster_genes , self.cfg.data.rna.clustering_threshold)
-            self.preproc.fit()
-            for split_name, dataset in splits.items():
-                splits[split_name] =self.preproc.transform_labels(dataset)
-                transforms = base_transforms(self.preproc.get_scaling())
-                datasets[split_name] =RNADataset(splits[split_name], self.cfg.base.rna_dataset_path, 
-                                                 transform = transforms, is_hazard_logits = True, column_order=self.preproc.get_column_order())
-            return datasets
-        else:
-            raise NotImplementedError("Exist only for rna. Initialising datasets for other modalities aren't declared")
     
     def initialise_models(self):
            if self.cfg.base.modalities[0]=="rna": 
@@ -80,30 +120,50 @@ class UnimodalSurvivalTrainer(object):
             self.scheduler.step()
             
         return  metrics  
-                
-            
-    def train(self, fold_ind : int):
+                    
+        
+        
+class UnimodalMAETrainer(Trainer):
     
-        for epoch in range(self.cfg.base.n_epochs):
-            self.model.train()
-            
-            train_metrics = self.__loop__("train",fold_ind, self.dataloaders['train'], self.cfg.base.device)
-            train_metrics.update({"epoch": epoch})
-            if self.cfg.base.log.logging:
-                wandb.log({f"train/fold_{fold_ind}/{key}" : value for key, value in train_metrics.items()})
-            
-            self.model.eval()
-            with torch.no_grad():    
-                val_metrics = self.__loop__("val",fold_ind, self.dataloaders['val'], self.cfg.base.device)
-            val_metrics.update({"epoch": epoch})    
-            if self.cfg.base.log.logging:
-                wandb.log({f"val/fold_{fold_ind}/{key}" : value for key, value in val_metrics.items()})
-        return val_metrics
+    def __init__(self, splits: Dict[str,pd.DataFrame], cfg: DictConfig):
+        super().__init__(splits, cfg)
+        transforms = padded_transforms(self.preproc.get_scaling(), cfg.model.rna_size)
+        self.datasets = self.initialise_datasets(splits, transforms)
+        self.dataloaders = {"train" : DataLoader(self.datasets["train"],shuffle=True, batch_size =cfg.base.batch_size),
+                            "val" : DataLoader(self.datasets["val"],shuffle=False, batch_size = 1),
+                            "test" : DataLoader(self.datasets["test"],shuffle=False, batch_size =1)
+                            }
+        self.model =self.initialise_models().to(cfg.base.device)
+        print(self.model)
+        self.initialise_loss()
     
-    def evaluate(self, fold_ind : int):
-            self.model.eval()
-            with torch.no_grad():    
-                test_metrics = self.__loop__("test",fold_ind, self.dataloaders['test'], self.cfg.base.device)
-            if self.cfg.base.log.logging:
-                wandb.log({f"test/fold_{fold_ind}/{key}" : value for key, value in test_metrics.items()})    
-            return test_metrics     
+    def initialise_loss(self):
+        self.optimizer = AdamW(self.model.parameters(), **self.cfg.base.optimizer.params)
+        self.scheduler = CosineAnnealingLR(self.optimizer, T_max=self.cfg.base.n_epochs,**self.cfg.base.scheduler.params)
+        
+    def initialise_models(self):
+        if self.cfg.base.modalities[0]=="rna":
+                return RnaMAEForPreTraining(ViTMAEConfig(**OmegaConf.to_container(self.cfg.model)))
+        else:
+            raise NotImplementedError("Exist only for rna. Initialising datasets for other modalities aren't declared") 
+        
+    def __loop__(self,split, fold_ind, dataloader, device):
+        total_loss =0
+        
+        for batch in dataloader:
+            data, time, event = batch  
+            outputs =self.model(data.to(device))
+            
+            if split=="train":
+                self.optimizer.zero_grad()
+                outputs.loss.backward()
+                self.optimizer.step()
+            
+            total_loss+=outputs.loss
+        
+        metrics = {"mse_loss": total_loss.cpu().detach().numpy() / len(dataloader.dataset)}
+        
+        if split=="train":
+            self.scheduler.step()
+            
+        return  metrics
