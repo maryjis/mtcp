e6208b9520160544c7f68d14f97fdffacfa81eda
Belyaeva Alex <66034483+BelyaevaAlex@users.noreply.github.com>
2025-01-21 01:47:35 +0300

    Update models.py

--- src/unimodal/wsi/models.py
+++ src/unimodal/wsi/models.py
@@ -33,7 +33,7 @@ class ResNetWrapperSimCLR(nn.Module):
 
 class WSIEncoder(nn.Module):
     """
-    A attention-based encoder for WSI data.
+    An attention-based encoder for WSI data.
     """
 
     def __init__(
@@ -47,34 +47,51 @@ class WSIEncoder(nn.Module):
         mlp_dim: int = 128,
         dropout: float = 0.0,
         emb_dropout: float = 0.0,
+        n_outputs: int = 20  # Параметр для количества выходных признаков
     ) -> None:
         super().__init__()
         self.layer_norm = nn.LayerNorm(dim)
 
-        # self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        # cls токен
         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
         self.dropout = nn.Dropout(emb_dropout)
 
+        # Трансформер
         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
+        # Пуллинг (cls или mean)
         self.pool = pool
+
+        # Преобразование в нужный размер
         self.to_latent = (
             nn.Identity() if embedding_dim == dim else nn.Linear(dim, embedding_dim)
         )
+
+        # Добавляем линейный слой для выходных признаков
+        self.output_layer = nn.Linear(embedding_dim, n_outputs)  # Новый слой для выхода
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.layer_norm(x)
         b, n, _ = x.shape
 
+        # Добавляем CLS токен
         cls_tokens = repeat(self.cls_token, "1 1 d -> b 1 d", b=b)
         x = torch.cat((cls_tokens, x), dim=1)
-        # x += self.pos_embedding[:, :(n + 1)]
+
+        # Применяем dropout
         x = self.dropout(x)
 
+        # Пропускаем через трансформер
         x = self.transformer(x)
 
+        # Выполняем пуллинг (выбор CLS токена или усреднение)
         x = x.mean(dim=1) if self.pool == "mean" else x[:, 0]
+        
+        # Преобразуем в нужный размер
         x = self.to_latent(x)
+
+        # Проходим через выходной слой
+        x = self.output_layer(x)
 
         return x
 
